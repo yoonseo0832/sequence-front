608ff3ceed413630b12f86951e5112e9926d8a3f
yoonseo0832 <yoonseo0832@gmail.com>
2024-12-04 18:04:45 +0900

    수정중

--- 윤서보경팀/3차과제/윤서/project/script.js
+++ 윤서보경팀/3차과제/윤서/project/script.js
@@ -1,1 +1,38 @@
-const container = document.querySelector(".experience-container");
+document.addEventListener("DOMContentLoaded", () => {
+    const container = document.getElementById("scroll-container");
+    const items = Array.from(container.children);
+    const itemWidth = items[0].offsetWidth + 40;
+
+    let scrollPosition = 0;
+    let animationFrame;
+
+    function cloneItems() {
+        const clonesNeeded = items.length;
+        for (let i = 0; i < clonesNeeded; i++) {
+            const clone = items[i % items.length].cloneNode(true);
+            container.appendChild(clone);
+        }
+    }
+
+    function startInfiniteScroll() {
+        scrollPosition += 8;
+
+        if (scrollPosition >= container.scrollWidth / 2) {
+            const firstItem = container.firstElementChild;
+            container.appendChild(firstItem);
+            scrollPosition -= itemWidth;
+        }
+
+        container.scrollLeft = scrollPosition;
+        animationFrame = requestAnimationFrame(startInfiniteScroll);
+    }
+
+    function stopInfiniteScroll() {
+        cancelAnimationFrame(animationFrame);
+    }
+
+    cloneItems();
+
+    container.addEventListener("mouseover", startInfiniteScroll);
+    container.addEventListener("mouseout", stopInfiniteScroll);
+});
